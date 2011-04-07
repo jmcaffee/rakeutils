5cc81fcf16fc0487e7b083129443ad74572cb81c
Jeff McAffee <jeff@ktechsystems.com>
2011-04-07 17:54:05 +0000

    Adding support for generating a gem for the project.
    Adding version.rb

--- rakefile.rb
+++ rakefile.rb
@@ -7,6 +7,9 @@
 # Website::   http://ktechsystems.com
 ######################################################################################
 
+require 'rubygems'
+require 'rake/gempackagetask'
+
 require 'rake'
 require 'rake/clean'
 require 'rake/rdoctask'
@@ -18,8 +21,16 @@ require 'ostruct'
 
 PROJNAME        = "RakeUtils"
 
+$:.unshift File.expand_path("../lib", __FILE__)
+require "rakeutils/version"
+
+PKG_VERSION	= RakeUtils::VERSION
+PKG_FILES 	= Dir["**/*"].select { |d| d =~ %r{^(README|bin/|data/|ext/|lib/|spec/|test/)} }
+
 # Setup common clean and clobber targets
 
+CLEAN.include("pkg")
+CLOBBER.include("pkg")
 #CLEAN.include("#{BUILDDIR}/**/*.*")
 #CLOBBER.include("#{BUILDDIR}")
 
@@ -61,3 +72,36 @@ Rake::RDocTask.new do |rdoc|
 end
 
 
+#############################################################################
+spec = Gem::Specification.new do |s|
+	s.platform = Gem::Platform::RUBY
+	s.summary = "Rake Utility classes"
+	s.name = PROJNAME.downcase
+	s.version = PKG_VERSION
+	s.requirements << 'none'
+	s.require_path = 'lib'
+	#s.autorequire = 'rake'
+	s.files = PKG_FILES
+	#s.executables = ""
+	s.author = "Jeff McAffee"
+	s.email = "gems@ktechdesign.com"
+	s.homepage = "http://gems.ktechdesign.com"
+	s.description = <<EOF
+Utility classes used in various rakefiles.
+EOF
+end
+
+
+#############################################################################
+Rake::GemPackageTask.new(spec) do |pkg|
+	pkg.need_zip = true
+	pkg.need_tar = true
+	
+	puts "PKG_VERSION: #{PKG_VERSION}"
+#=begin		
+	puts "PKG_FILES:"
+	PKG_FILES.each do |f|
+		puts "  #{f}"
+	end
+#=end
+end
