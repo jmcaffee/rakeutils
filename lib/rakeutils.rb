d4dcfa20dcac4c484969659ab6431b4a0d1463f8
Jeff McAffee <jeff@ktechsystems.com>
2015-07-14 16:47:21 -0700

    Replace dir searching for requires
    Use direct implicit require statements instead.

--- lib/rakeutils.rb
+++ lib/rakeutils.rb
@@ -1,21 +1,35 @@
-######################################################################################
+###############################################################################
+# File:       rakeutils.rb
+# Purpose:    rakeutils master include file
 #
-# rakeUtils.rb
-#
-# Jeff McAffee   10/25/08
-#
-# Purpose: rakeUtils master include file
-#
-######################################################################################
+# Author:     Jeff McAffee 10/25/2008
+# Copyright:  Copyright (c) 2008, Jeff McAffee
+#             All rights reserved. See LICENSE.txt for details.
+# Website:    http://JeffMcAffee.com
+##############################################################################
+require 'rakeutils/version'
 
-# Require source files
+module RakeUtils
 
-require 'find'
+end
+
 require 'logger'
 
-class_files = File.join( File.dirname(__FILE__), 'rakeutils', '*.rb')
-$: << File.join( File.dirname(__FILE__), 'rakeutils')                   # Add directory to the include file array
-Dir.glob(class_files) do | class_file | 
-  require class_file[/\w+\.rb$/]
-end
+require 'rakeutils/clapp'
+require 'rakeutils/filegentask'
+require 'rakeutils/innotask'
+require 'rakeutils/javacctask'
+require 'rakeutils/jjtreetask'
+require 'rakeutils/ocratask'
+require 'rakeutils/text2rtf'
+require 'rakeutils/versioninc'
+require 'rakeutils/ziptask'
+
+#require 'find'
+#
+#class_files = File.join( File.dirname(__FILE__), 'rakeutils', '*.rb')
+#$: << File.join( File.dirname(__FILE__), 'rakeutils')                   # Add directory to the include file array
+#Dir.glob(class_files) do | class_file | 
+#  require class_file[/\w+\.rb$/]
+#end
 
