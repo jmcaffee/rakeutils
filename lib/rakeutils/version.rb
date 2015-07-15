66efdbb6896d3af0400d17d778dcfc48a5884e62
Jeff McAffee <jeff@ktechsystems.com>
2015-07-15 16:35:28 -0700

    Bump version to v0.1.0

--- lib/rakeutils/version.rb
+++ lib/rakeutils/version.rb
@@ -1,5 +1,5 @@
 module RakeUtils
-  VERSION = "0.0.3" unless constants.include?("VERSION")
+  VERSION = "0.1.0" unless constants.include?("VERSION")
   APPNAME = "RakeUtils" unless constants.include?("APPNAME")
   COPYRIGHT = "Copyright (c) 2014, kTech Systems LLC. All rights reserved" unless constants.include?("COPYRIGHT")
 end
